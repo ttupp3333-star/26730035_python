0b606085249ae71c44f30f3156a5011576ac4c62
ttupp3333-star <ttupp3333@gmail.com>
2026-09-17 12:24:18 +0900

    Add files via upload

---
+++ 3주차/2. 별찍기.py
@@ -0,0 +1,32 @@
+#별찍기
+'''
+*
+**
+***
+****
+*****
+'''
+
+
+
+
+
+for i in range(1,6):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+print()
+
+'''
+*****
+****S
+***
+**
+*
+'''
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
---
+++ 3주차/제출 3. 그림.py
@@ -0,0 +1,9 @@
+def printPattern(rows=5, cols=5, char="*"):
+    for _ in range(rows):
+        for _ in range (cols):
+            print(char, end="")
+        print()
+printPattern(3,4,"&")
+
+    
+  
